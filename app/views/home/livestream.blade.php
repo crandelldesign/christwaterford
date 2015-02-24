f68c0796d4c973c9bdd9921736d717482ae00aea
Matt Crandell <mrcrandell@gmail.com>
2015-02-23 20:52:39 -0500

    Updated livestream.

--- app/views/home/livestream.blade.php
+++ app/views/home/livestream.blade.php
@@ -3,14 +3,14 @@
 
 <h1>Livestream</h1>
 
+<iframe src="http://new.livestream.com/accounts/8727672/events/3834858/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
+
+<iframe src="http://new.livestream.com/accounts/8727672/events/3817931/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
+
 <iframe src="http://new.livestream.com/accounts/8727672/events/3797982/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
 
 <iframe src="http://new.livestream.com/accounts/8727672/events/3778781/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
 
 <iframe src="http://new.livestream.com/accounts/8727672/events/3760395/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
-
-<iframe src="http://new.livestream.com/accounts/8727672/events/3740171/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
-
-<iframe src="http://new.livestream.com/accounts/8727672/events/3721504/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
 
 @stop
