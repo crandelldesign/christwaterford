307b1174d24144da96a20287b80e23e2fb87fec1
Matt Crandell <mrcrandell@gmail.com>
2015-10-20 17:55:15 -0400

    Updated livestream

--- app/views/home/livestream.blade.php
+++ app/views/home/livestream.blade.php
@@ -5,19 +5,19 @@
 
 <p>See our worship services live right here! You can also view recent past services in case you missed them live.</p>
 
+<h2>Lutheran Worship 10-25-15</h2>
+<iframe src="http://livestream.com/accounts/8727672/events/4439143/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
+
 <h2>Lutheran Worship 10-18-15</h2>
 <iframe src="http://livestream.com/accounts/8727672/events/4420197/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
+
+<h2>Lutheran Worship 10-11-15</h2>
+<iframe src="http://livestream.com/accounts/8727672/events/4404032/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
 
 <h2>Lutheran Worship 10-4-15</h2>
 <iframe src="http://livestream.com/accounts/8727672/events/4382641/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
 
 <h2>Lutheran Worship 9-27-15</h2>
 <iframe src="http://livestream.com/accounts/8727672/events/4369458/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
-
-<h2>Lutheran Worship 9-20-15</h2>
-<iframe src="http://livestream.com/accounts/8727672/events/4347326/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
-
-<h2>Lutheran Worship 9-13-15</h2>
-<iframe src="http://livestream.com/accounts/8727672/events/4330371/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no"> </iframe>
 
 @stop
