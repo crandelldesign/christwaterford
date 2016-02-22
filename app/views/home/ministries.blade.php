e9e553d3dcdaba90c687b8d2ec126f4c31ce2da6
Matt Crandell <mrcrandell@gmail.com>
2016-02-21 23:25:56 -0500

    Installed Laravel 5.1. Set up elixir. Began adding dependencies.

--- app/views/home/ministries.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('master.templates.master', array('var1'=>'', 'var2'=>''))
-@section('body')
-
-<h1>Ministries and Teams</h1>
-
-@stop
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+	// Build Stylesheet
+    mix.sass(
+		'stylesheet.scss',
+		'public/css/stylesheet.css',
+		{
+	        includePaths: [
+	            'vendor/twbs/bootstrap-sass/assets/stylesheets/',
+	            'vendor/fortawesome/font-awesome/scss/'
+	        ]
+      	}
+    );
+    // Copy Bootstrap's JS
+    mix.copy('vendor/twbs/bootstrap-sass/assets/javascripts/*.js', 'resources/assets/js/bootstrap');
+    // Build JS
+    mix.scripts(
+		[
+	        'jquery-2.2.0.min.js',
+	        'bootstrap/bootstrap.js',
+	        'master.js'
+		],
+		'public/js/master.js'
+    );
+    // Copy Fonts
+    mix.copy('vendor/twbs/bootstrap-sass/assets/fonts', 'public/fonts/bootstrap');
+	mix.copy('vendor/fortawesome/font-awesome/fonts', 'public/fonts');
+
+	// Create Build Numbers
+    mix.version(['css/stylesheet.css', 'js/master.js']);
+
+    // Copy Fonts and Images for Build Numbers
+    mix.copy('vendor/twbs/bootstrap-sass/assets/fonts', 'public/build/fonts/bootstrap');
+	mix.copy('vendor/fortawesome/font-awesome/fonts', 'public/build/fonts');
+    mix.copy('public/img', 'public/build/img');
+});
