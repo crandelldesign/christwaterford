231f2e5e11436d72ad55fe8282696082983490e5
Matt Crandell <mrcrandell@gmail.com>
2016-04-21 22:09:38 -0400

    Built style guide.

--- resources/views/style.blade.php
+++ resources/views/style.blade.php
@@ -2,11 +2,20 @@
 <html>
     <head>
         <meta charset="utf-8">
-        @include('master.templates.head')
+        <title>Christ Lutheran Church of Waterford</title>
+        <meta name="description" content="">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <link rel="stylesheet" type="text/css" href="{{ elixir('css/stylesheet.css') }}" />
     </head>
     <body>
         <header class="style-header">
-            <h1>Christ Lutheran Church of Waterford</h1>
+            <div class="container-fluid">
+                <img src="{{url('/')}}/img/clc-logo.svg" alt="Christ Lutheran Church of Waterford" class="logo img-responsive">
+                <h1>Christ Lutheran Church <small>of Waterford</small></h1>
+            </div>
         </header>
         <div class="container-fluid page">
             <div class="row">
@@ -15,58 +24,63 @@
 
                     <div class="row">
                         <div class="col-md-4">
-                            <div class="color-swatch">
-                                Color Name<br>
-                                #FFFFFF;
+                            <div class="color-swatch swatch1">
+                                Dark Blue<br>
+                                #003366
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="color-swatch">
-                                Color Name<br>
-                                #FFFFFF;
+                            <div class="color-swatch swatch2">
+                                Green Blue<br>
+                                #00a99d
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="color-swatch">
-                                Color Name<br>
-                                #FFFFFF;
+                            <div class="color-swatch swatch3">
+                                Gray<br>
+                                #8b8c84
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="color-swatch">
-                                Color Name<br>
-                                #FFFFFF;
+                            <div class="color-swatch swatch4">
+                                Dark Green Blue<br>
+                                #00746b
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="color-swatch">
-                                Color Name<br>
-                                #FFFFFF;
+                            <div class="color-swatch swatch5">
+                                Green Blue 2<br>
+                                #1cbbb4
                             </div>
                         </div>
                         <div class="col-md-4">
-                            <div class="color-swatch">
-                                Color Name<br>
-                                #FFFFFF;
+                            <div class="color-swatch swatch6">
+                                Black<br>
+                                #252525
                             </div>
                         </div>
                     </div>
+                    <hr>
+                    <h1>Buttons</h1>
+
+                    <button class="btn btn-lg btn-blue">This is a large button</button>
+                    <button class="btn btn-darkgreenblue">This is another smaller button</button>
                 </div>
                 <div class="col-sm-6">
                     <h1>Typography</h1>
 
                     <h1>Heading 1</h1>
-                    <p class="details">Styling Details</p>
+                    <p class="details">Font: EB Garamond #003366</p>
 
                     <hr>
 
                     <h2>Heading 2</h2>
-                    <p class="details">Styling Details</p>
+                    <p class="details">Font: Open Sans Semibold #00746b</p>
 
                     <hr>
 
                     <h3>Heading 3</h3>
-                    <p class="details">Styling Details</p>
+                    <p class="details">Font: Open Sans Regular #252525</p>
 
                     <hr>
 
@@ -74,24 +88,22 @@
                     <p>Lorem ipsum dolor sit amet, quis quam, fusce duis. Montes vestibulum esse, tristique dui lorem. Wisi cubilia. Nonummy justo, eros aliquet elit, nulla sollicitudin ut. Iaculis sit lacus, nisi orci nunc, pede convallis vestibulum.</p>
                     <p>Sed tellus. Posuere est quis, lacus sit nec. Ultricies vehicula arcu, nunc nonummy id. Vivamus odio neque, faucibus duis. Non diam amet, elit nec semper.</p>
 
-                    <p><a href="#">This is a link</a> <a href="#" class="hover">This is a hover link</a></p>
+                    <p><a href="#">This is a link</a> - <a href="#" class="hover">This is a hover link</a></p>
 
-                    <p class="details">Styling Details</p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
-                    <h1>Buttons</h1>
-
-                    <button class="btn btn-blue">This is a button</button>
-                    <button class="btn btn-blue">This is another button</button>
+                    <p class="details">Font: Open Sans Regular #252525</p>
                 </div>
             </div>
         </div>
 
+        <footer class="footer">
+            <div class="container-fluid">
+                <p>Copyright &copy; {{date('Y')}} Christ Lutheran Church of Waterford. All Rights Reserved.</p>
+                <p>Designated trademarks and brands are the property of their respective owners.</p>
+                <p>All graphics are created by Matt Crandell of <a href="http://www.crandelldesign.com" target="_blank">Crandell Design</a>.</p>
+            </div>
+        </footer>
         <script type="text/javascript" src="{{ elixir('js/master.js') }}"></script>
-        @include('master.templates.footer')
-        @yield('scripts')
+        
     </body>
     
 </html>
