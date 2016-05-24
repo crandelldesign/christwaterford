e8bfebaebad0c49abac798556f1cc193dd62fee0
Crandell, Matt <mcrandell@jrthompson.com>
2016-05-24 14:40:16 -0400

    Updated homepage. Added slideshow files.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,28 @@
 @extends('layouts.default')
 @section('content')
+     
+<h1>What makes a church special?</h1>
+<h2>It's people like you!</h2>
+
+<div class="row">
+    <div class="col-sm-6">
+        <h2>You will find us at</h2>
+        <p>5987 Williams Lake Road<br>
+            (at Airport Road, one mile west of Dixie Highway)<br>
+            Waterford MI 48329</p>
+        <p>Go to the <a href="http://christwaterford.org/contact">Contact Us</a> page for more information and a map.</p>
+        <h2>Sunday Services</h2>
+        <p>We worship at 10:00 AM on Sundays</p>
+        <p>We also have special service times during the Christmas and the Lenten/Easter season as well as Thanksgiving eve.</p>
+        <p>You will find that we have a "blended" service, incorporating some traditional Lutheran liturgy, bible readings, prayer, children's message and sermon along with lots of opportunities for congregational singing, incorporating both traditional and contemporary songs with the Chancel Choir, Chapel Bell Choir and soloists often contributing to the service. We have open communion every Sunday and all believing Christians are welcome to commune.</p>
+        <h2>Our Mission</h2>
+        <p>We are a community of God's people called to share the love of God as made known in Jesus Christ and to be transformed by that love.</p>
+    </div>
+    <div class="col-sm-6">
+        <h2>Upcoming Events</h2>
+    </div>
+</div>
+
+<p class="tagline">Where CLC also means &quot;Caring, Loving Community.&quot;</p>
+
 @stop
