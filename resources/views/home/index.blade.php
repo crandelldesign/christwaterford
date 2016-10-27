b76b567fd72d3c5a5f2d1bfc0bc0dc22a5bbc685
Crandell, Matt <mcrandell@jrthompson.com>
2016-10-27 07:57:58 -0400

    Added pause to slidehsow when clicked.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -69,6 +69,10 @@
             prevArrow: '<button type="button" class="slideshow-prev"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-angle-left fa-stack-1x"></i></span></button>',
             nextArrow: '<button type="button" class="slideshow-next"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-angle-right fa-stack-1x"></i></span></button>'
         });
+        $('.slideshow-prev, .slideshow-next').on('click', function(event)
+        {
+            $('.slideshow').slick('slickPause');
+        });
     });
 </script>
 @stop
