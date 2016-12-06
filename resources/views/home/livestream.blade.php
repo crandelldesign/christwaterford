26ab08da5af0f1b9f9984dce1190f8f72205c1e4
Matt Crandell <mrcrandell@gmail.com>
2016-12-05 21:12:51 -0500

    Updated livestream.

--- resources/views/home/livestream.blade.php
+++ resources/views/home/livestream.blade.php
@@ -8,6 +8,11 @@
     <div class="col-md-8">
         <p>See our worship services live right here! You can also view recent past services in case you missed them.</p>
 
+        <h2>Lutheran Worship 12-11-16</h2>
+        <div class="embed-responsive embed-responsive-16by9">
+        <iframe class="embed-responsive-item" id="ls_embed_1480977680" src="https://livestream.com/accounts/8727672/events/6726645/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
+        </div>
+
         <h2>Lutheran Worship 11-20-16</h2>
         <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" id="ls_embed_1479135592" src="https://livestream.com/accounts/8727672/events/6639476/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no" allowfullscreen></iframe>
@@ -26,11 +31,6 @@
         <h2>Lutheran Worship 10-23-16</h2>
         <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" id="ls_embed_1476967867" src="//livestream.com/accounts/8727672/events/6523591/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no" allowfullscreen></iframe>
-        </div>
-
-        <h2>Lutheran Worship 10-9-16</h2>
-        <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" id="ls_embed_1475509170" src="//livestream.com/accounts/8727672/events/6445741/player?width=560&height=315&autoPlay=true&mute=false" width="560" height="315" frameborder="0" scrolling="no" allowfullscreen></iframe>
         </div>
 
         {{--<!--<h2>Richard Nelson Memorial Service</h2>
