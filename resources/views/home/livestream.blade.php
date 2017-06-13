16bbf43c5c0c59d7a6d8b0e2e49fccd036ba9ad6
Matt Crandell <mrcrandell@gmail.com>
2017-06-13 17:36:47 -0400

    Fixed broken div.

--- resources/views/home/livestream.blade.php
+++ resources/views/home/livestream.blade.php
@@ -10,6 +10,7 @@
 
         <div itemscope itemtype="http://schema.org/Event">
         <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2017-06-18">6-18-17</span></h2>
+        <div class="embed-responsive embed-responsive-16by9">
         <iframe class="embed-responsive-item" id="ls_embed_1497311848" src="https://livestream.com/accounts/8727672/events/7502447/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
         </div>
         </div>
