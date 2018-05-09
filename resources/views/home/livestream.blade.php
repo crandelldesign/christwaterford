f01617289662b75ffc0500611996377d0c344c75
Matt Crandell <mrcrandell@gmail.com>
2018-05-09 19:04:37 -0400

    Updated lievstream date.

--- resources/views/home/livestream.blade.php
+++ resources/views/home/livestream.blade.php
@@ -16,7 +16,7 @@
         </div>
         
         <div itemscope itemtype="http://schema.org/Event">
-            <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2018-04-29">4-29-18</span></h2>
+            <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2018-05-06">5-6-18</span></h2>
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" id="ls_embed_1524458138" src="https://livestream.com/accounts/8727672/events/8172263/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
             </div>
