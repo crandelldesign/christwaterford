1836d27250c8ec7d5bc9c3444fcaac5f63c3c978
Matt Crandell <mrcrandell@gmail.com>
2019-08-08 19:57:51 -0400

    Updated livestream.

--- resources/views/home/livestream.blade.php
+++ resources/views/home/livestream.blade.php
@@ -9,6 +9,13 @@
         <p>See our worship services live right here! You can also view recent past services in case you missed them.</p>
 
         <div itemscope itemtype="http://schema.org/Event">
+            <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2019-08-11">8-11-19</span></h2>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" id="ls_embed_1565176435" src="https://livestream.com/accounts/8727672/events/8774527/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
+            </div>
+        </div>
+        
+        <div itemscope itemtype="http://schema.org/Event">
             <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2019-08-04">8-4-19</span></h2>
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" id="ls_embed_1564451961" src="https://livestream.com/accounts/8727672/events/8765872/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
@@ -33,13 +40,6 @@
             <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2019-07-14">7-14-19</span></h2>
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" id="ls_embed_1562646854" src="https://livestream.com/accounts/8727672/events/8743638/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
-            </div>
-        </div>
-        
-        <div itemscope itemtype="http://schema.org/Event">
-            <h2 itemprop="name">Lutheran Worship <span itemprop="startDate" content="2019-06-30">6-30-19</span></h2>
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" id="ls_embed_1561377361" src="https://livestream.com/accounts/8727672/events/8728396/player?width=640&height=360&enableInfoAndActivity=true&defaultDrawer=&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no" allowfullscreen> </iframe>
             </div>
         </div>
 
